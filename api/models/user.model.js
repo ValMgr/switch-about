a193af2aa71b06cceaa7160014d70e9975c4516c
Valentin Magry <valou32720@gmail.com>
2022-06-16 15:44:50 +0200

    Add: user model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,26 +1,90 @@
-// module.exports = {
+const db = require('../config/db');
 
-//   getAllUsers(req){
-//     db.query(`SELECT * FROM users`, (err, results) => {
-//       if (err) {
-//         res.status(500).send(err);
-//       } else {
-//         res.status(200).send(results);
-//       }
-//     });
-//   }
+const User = (user) => {
+    this.firstname = user.firstname;
+    this.lastname = user.lastname;
+    this.age = user.age;
+    this.phone = user.phone;
+    this.email = user.email;
+    this.password = user.password;
+}
 
-// }
+User.findAll = (result) => {
+    db.query('SELECT * FROM user', (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+        
+        console.log("users: ", res);
+        result(null, res);
+    });
+}
 
+User.findById = (id, result) => {
+    db.query('SELECT * FROM user WHERE id = ?', id, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
+        }
 
+        if (res.length) {
+            console.log("found user: ", res[0]);
+            result(null, res[0]);
+            return;
+        }
 
-// getUserByAttribute(req, res){
-//   const { attribute, value } = req.params;
-//   db.query(`SELECT * FROM users WHERE ${attribute} = ?`, [value], (err, results) => {
-//     if (err) {
-//       res.status(500).send(err);
-//     } else {
-//       res.status(200).send(results);
-//     }
-//   });
-// }
+        result({ kind: "not_found" }, null);
+    });
+}
+
+User.findByEmail = (email, result) => {
+    db.query('SELECT * FROM user WHERE email = ?', email, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
+        }
+
+        if (res.length) {
+            console.log("found user: ", res[0]);
+            result(null, res[0]);
+            return;
+        }
+
+        result({ kind: "not_found" }, null);
+    });
+}
+
+User.create = (user, result) => {
+    db.query("INSERT INTO user SET ?", user, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
+        }
+        
+        console.log("created user: ", { id: res.insertId, ...user });
+        result(null, { id: res.insertId, ...user });
+    });
+}
+
+User.delete = (id, result) => {
+    db.query("DELETE FROM user WHERE id = ?", id, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+
+        if (res.affectedRows == 0) {
+            result({ kind: "not_found" }, null);
+            return;
+        }
+
+        console.log("deleted user with id: ", id);
+        result(null, res);
+    });
+}
