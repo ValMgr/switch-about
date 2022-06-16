b17ed3bbb628b323f8f6fc5d7b2f7de086c5f895
Valentin Magry <valou32720@gmail.com>
2022-06-16 12:29:24 +0200

    Fix: reorganize files

--- api/server.js
+++ api/server.js
@@ -1,32 +1,33 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
 const mysql = require("mysql");
 const db_loader = require("./db/db_loader");
 
-const db = mysql.createConnection({
-  host: "db",
-  user: "root",
-  password: "password",
-  database: "db",
-  port: "3306",
-});
+const app = express();
+const db = require("./config/db");
 
-db.connect(function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Connected to database");
-  }
-});
+
 
 app.get("/", (req, res) => {
-  // Send if the database is connected
-  res.send("Database is connected with id " + db.threadId);
+  
 });
+
+app.get("/formation", (req, res) => {
+  db.query("SELECT * FROM FORMATION", (err, result) => {
+    if (err) {
+      console.log("error: ", err);
+      res.sendStatus(500);
+      return;
+    }
+    res.send(result);
+    });
+});
+
+
 
 app.get("/load", (req, res) => {
   db_loader.loadDB("./db/db.csv", db);
-  res.send("Loaded DB");
+  res.send("<h1>Loaded DB !</h1> <a href='/'>Back home</a>");
 });
 
 const PORT = 3000;
