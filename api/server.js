adfc4472ba51f58f7172f6296f4b503d2138c0c3
Valentin Magry <valou32720@gmail.com>
2022-06-17 11:07:23 +0200

    Add: automatically load db if empty

--- api/server.js
+++ api/server.js
@@ -12,12 +12,17 @@ require("./routes/profil.routes")(app);
 
 app.get("/", (req, res) => res.send("SwitchAbout - API v1.0.0"));
 
-app.get("/load", (req, res) => {
-  db_loader.loadDB("./db/db.csv", db);
-  res.send("<h1>Loaded DB !</h1> <a href='/'>Back home</a>");
-});
-
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
+  db.query("SELECT * FROM FORMATION", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      return;
+    }
+    if (!res.length) {
+      db_loader.loadDB("./db/db.csv", db);
+      console.log("DB loaded");
+    }
+  });
 });
