326a3d9c46cf8cfdc77cf6923817fef57c06347b
keshiamukenge <mukenge.keshia@gmail.com>
2022-06-17 15:56:05 +0200

    feat: add menu component without style

--- app/src/pages/Formations.js
+++ app/src/pages/Formations.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { FormationsList, Loader, Error } from '../components';
 import { getFormationsApi } from '../services.api';
 import { PageContainer } from './styledComponents';
+import { AdminMenu } from './components/';
 
 export function Formations() {
   const [formations, setFormations] = useState();
@@ -29,6 +30,7 @@ export function Formations() {
   if(error) {
     return (
       <PageContainer>
+        <AdminMenu />
         <Error message="Une erreur est survenue lors de la récupération des formations. Veuillez recharger la page." />
       </PageContainer>
     );
@@ -37,6 +39,7 @@ export function Formations() {
   if(loading && !error) {
     return (
       <PageContainer>
+        <AdminMenu />
         <Loader message="Récupération de la liste des formations en cours..." />
       </PageContainer>
     );
@@ -44,6 +47,7 @@ export function Formations() {
 
   return(
     <PageContainer>
+      <AdminMenu />
       <FormationsList formations={formations} />
     </PageContainer>
   );
